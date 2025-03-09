e062b35b9ccdf72d719a8857ee42bcd72e7883d9
Rajkumaran S <148076615+Rajkumaran-S@users.noreply.github.com>
2025-03-09 08:59:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const taskInput = document.querySelector(".task-input input"),
   filters = document.querySelectorAll(".filters span"),
   clearAll = document.querySelector(".clear-btn"),
   taskBox = document.querySelector(".task-box");
-// Initialize variables
+
 let editId,
   isEditTask = false,
   todos = JSON.parse(localStorage.getItem("todo-list"));
-// Event listeners for filter buttons
+
 filters.forEach(btn => {
   btn.addEventListener("click", () => {
     document.querySelector("span.active").classList.remove("active");
@@ -14,15 +14,15 @@ filters.forEach(btn => {
     showTodo(btn.id);
   });
 });
-// Function to display tasks based on filter
+
 function showTodo(filter) {
   let liTag = "";
   if (todos) {
     todos.forEach((todo, id) => {
-      // Determine if task is completed or pending
+     
       let completed = todo.status == "completed" ? "checked" : "";
       if (filter == todo.status || filter == "all") {
-        // Create task list item
+        
         liTag += `<li class="task">
                             <label for="${id}">
                                 <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
@@ -39,27 +39,27 @@ function showTodo(filter) {
       }
     });
   }
-  // Update task box with tasks or a message if no tasks
+ 
   taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
   let checkTask = taskBox.querySelectorAll(".task");
-  // Toggle clear all button based on task presence
+
   !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
-  // Add or remove overflow class based on task box height
+
   taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
 }
 showTodo("all");
-// Function to display menu for task actions
+
 function showMenu(selectedTask) {
   let menuDiv = selectedTask.parentElement.lastElementChild;
   menuDiv.classList.add("show");
-  // Hide menu if clicked outside
+
   document.addEventListener("click", e => {
     if (e.target.tagName != "I" || e.target != selectedTask) {
       menuDiv.classList.remove("show");
     }
   });
 }
-// Function to update task status (completed or pending)
+
 function updateStatus(selectedTask) {
   let taskName = selectedTask.parentElement.lastElementChild;
   if (selectedTask.checked) {
@@ -71,7 +71,7 @@ function updateStatus(selectedTask) {
   }
   localStorage.setItem("todo-list", JSON.stringify(todos))
 }
-// Function to prepare for editing a task
+
 function editTask(taskId, textName) {
   editId = taskId;
   isEditTask = true;
@@ -79,21 +79,21 @@ function editTask(taskId, textName) {
   taskInput.focus();
   taskInput.classList.add("active");
 }
-// Function to delete a task
+
 function deleteTask(deleteId, filter) {
   isEditTask = false;
   todos.splice(deleteId, 1);
   localStorage.setItem("todo-list", JSON.stringify(todos));
   showTodo(filter);
 }
-// Event listener for clear all button
+
 clearAll.addEventListener("click", () => {
   isEditTask = false;
   todos.splice(0, todos.length);
   localStorage.setItem("todo-list", JSON.stringify(todos));
   showTodo()
 });
-// Event listener for task input
+
 taskInput.addEventListener("keyup", e => {
   let userTask = taskInput.value.trim();
   if (e.key == "Enter" && userTask) {
